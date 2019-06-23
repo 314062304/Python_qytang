6251a98ac74b350b35ee1bea9b6510a1e0601906
314062304 <314062304@qq.com>
2019-06-23 18:16:41 +0800

    commit_str_qiepian2

--- str_qiepian.py
+++ str_qiepian.py
@@ -1,3 +1,12 @@
-word = 'scallywag'
-sub_word = word[2:-3]
-print(sub_word)
+# word = 'scallywag'
+# sub_word = word[2:-3]
+# print(sub_word)
+
+
+
+Hero = 'The Avengers'
+Man = Hero[0]
+Woman = Hero[1:-1]
+last = Hero[1]
+Heros = Woman + '-' + Man + last
+print(Heros)
