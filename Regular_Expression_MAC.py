39cefa3ab44211df7fdd28355fb18307fc71ec4e
314062304 <314062304@qq.com>
2019-06-24 22:46:11 +0800

    commit_Regular1

---
+++ Regular_Expression_MAC.py
@@ -0,0 +1,12 @@
+# 正则表达式测试1
+import re
+str1 = '166 54a2.74f7.0326 DYNAMIC Gi1/0/11'
+
+result = re.match('\s*(\w*)\s+(\w.*\d{1,4})\s+(\w*)\s+(\w*[^a]\w*[^a]\w*)\s*', str1).groups()
+
+print(result)
+print('-'*80)
+print('%-10s : %s' % ('VLAN ID', result[0]))
+print('%-10s : %s' % ('MAC', result[1]))
+print('%-10s : %s' % ('Type', result[2]))
+print('%-10s : %s' % ('Interface',  result[3]))
